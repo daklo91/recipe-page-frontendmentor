b593e903a98d560a8f0810982691ccdffbddb13b
daklo91 <coffeedog91@gmail.com>
2024-04-20 17:17:47 +0200

    Add custom colors and semi-bold outfit font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,37 @@ module.exports = {
   content: ["index.html", "main.css"],
   theme: {
     fontFamily: {
-      "outfit-bold": [
-        '"Outfit", sans-serif',
-        {
-          fontVariationSettings: '"wght" 700',
-        },
-      ],
       "outfit-regular": [
         '"Outfit", sans-serif',
         {
           fontVariationSettings: '"wght" 400',
         },
       ],
-      YoungSerif: ["YoungSerif", "serif"],
+      "outfit-semi-bold": [
+        '"Outfit", sans-serif',
+        {
+          fontVariationSettings: '"wght" 600',
+        },
+      ],
+      "outfit-bold": [
+        '"Outfit", sans-serif',
+        {
+          fontVariationSettings: '"wght" 700',
+        },
+      ],
+      YoungSerif: ['"YoungSerif", serif'],
+    },
+    colors: {
+      // Primary Colors
+      Nutmeg: "hsl(14, 45%, 36%)",
+      DarkRaspberry: "hsl(332, 51%, 32%)",
+      //Neutral Colors
+      White: "hsl(0, 0%, 100%)",
+      RoseWhite: "hsl(330, 100%, 98%)",
+      Eggshell: "hsl(30, 54%, 90%)",
+      LightGrey: "hsl(30, 18%, 87%)",
+      WengeBrown: "hsl(30, 10%, 34%)",
+      DarkCharcoal: "hsl(24, 5%, 18%)",
     },
     extend: {},
   },
