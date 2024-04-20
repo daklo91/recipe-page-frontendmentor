f4bd543f347dda1bb57f2f84549925d9fa3766b1
daklo91 <coffeedog91@gmail.com>
2024-04-20 16:57:19 +0200

    Add custom font to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 module.exports = {
   content: ["index.html", "main.css"],
   theme: {
+    fontFamily: {
+      "outfit-bold": [
+        '"Outfit", sans-serif',
+        {
+          fontVariationSettings: '"wght" 700',
+        },
+      ],
+      "outfit-regular": [
+        '"Outfit", sans-serif',
+        {
+          fontVariationSettings: '"wght" 400',
+        },
+      ],
+      YoungSerif: ["YoungSerif", "serif"],
+    },
     extend: {},
   },
   plugins: [],
